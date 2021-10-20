ed9f2aacbc0658ded48ef64dd3b4ba349d07c6e9
juanoviedo <juan.oviedo.lutkens@gmail.com>
2021-10-20 14:38:46 -0500

    Update production.py
    procfile -> Procfile

--- mvp_landing/settings/production.py
+++ mvp_landing/settings/production.py
@@ -32,7 +32,7 @@ DEBUG = False
 MY_VAR = "this_is_awesome"
 API_KEY = os.environ.get("THIS_API_KEY")
 
-ALLOWED_HOSTS = [".mvplanding.com"]
+ALLOWED_HOSTS = ["alcon-de-colombia.herokuapp.com"]
 
 
 # Application definition
