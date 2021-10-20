19d840ec8425872e2b37f1af784f35c68ecf585c
juanoviedo <juan.oviedo.lutkens@gmail.com>
2021-10-20 17:09:35 -0500

    Url config for /admin styling

--- mvp_landing/urls.py
+++ mvp_landing/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
+
 
 from django.urls import path, re_path
 
@@ -50,5 +53,4 @@ urlpatterns = [
 
 if settings.DEBUG: # denotes in DEVELOPMENT not PRODUCTION!!!
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
