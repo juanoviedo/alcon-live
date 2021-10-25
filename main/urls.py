d39df5ca986f2e86853bf6d9b0be61b0d5a7a64c
juanoviedo <juan.oviedo.lutkens@gmail.com>
2021-10-25 17:42:57 -0500

    Almost all links done

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('ganado/', views.ganado, name="ganado"),
+    path('home/', views.home, name="home"),
+    path('avicultura/', views.avicultura, name="avicultura"),
+    path('equinos/', views.equinos, name="equinos"),
+    path('ganaderia/', views.ganaderia, name="ganaderia"),
+    path('porcicultura/', views.porcicultura, name="porcicultura"),
+    path('mascotas/', views.mascotas, name="mascotas"),
+    path('cunicultura/', views.cunicultura, name="cunicultura"),
+    path('acuicultura/', views.acuicultura, name="acuicultura"),
+    path('vidinst/', views.vidinst, name="vidinst"),
+    path('puntosventa/', views.puntosventa, name="puntosventa"),
+    path('contactenos/', views.contactenos, name="contactenos"),
 ]
--- main/views.py
+++ main/views.py
@@ -7,5 +7,32 @@ from django.http import HttpResponse, HttpResponseRedirect
 def home(response):
     return render(response, "main/home.html", {})
 
-def ganado(response):
-    return render(response, "main/ganado.html", {})
+def avicultura(response):
+    return render(response, "main/avicultura.html", {})
+
+def equinos(response):
+    return render(response, "main/equinos.html", {})
+
+def ganaderia(response):
+    return render(response, "main/ganaderia.html", {})
+
+def porcicultura(response):
+    return render(response, "main/porcicultura.html", {})
+
+def mascotas(response):
+    return render(response, "main/mascotas.html", {})
+
+def cunicultura(response):
+    return render(response, "main/cunicultura.html", {})
+
+def acuicultura(response):
+    return render(response, "main/acuicultura.html", {})
+
+def vidinst(response):
+    return render(response, "main/vidinst.html", {})
+
+def puntosventa(response):
+    return render(response, "main/puntosventa.html", {})
+
+def contactenos(response):
+    return render(response, "main/contactenos.html", {})
